393f8d2fc65c52db14208790d286223214fec426
DavidH <51868751+daveh07@users.noreply.github.com>
2021-08-24 09:44:29 +1000

    Update design_calcs.py

--- modules/design_calcs.py
+++ modules/design_calcs.py
@@ -1,25 +1,31 @@
-#--------------------USER INPUTS------------------------#
-#Calculate the Axial Capacity of R.C wall in accordance with AS3600:2018, Section 11 for walls subject to
+# --------------------USER INPUTS------------------------#
+# Calculate the Axial Capacity of R.C wall in accordance with AS3600:2018, Section 11 for walls subject to
 # in-plane shear forces
 
-#import modules and packages
-import modules.inputs as inpts
-from modules.inputs import WallDetails, ReinfDetails, DesignLoads
+# Import modules and packages
+import modules.inputs as i
+from modules.inputs import *
 import math
 
 
 class WallAxialCapacity:
     def __init__(self, phi_factor):
+        super().__init__()
         self.phi_factor = phi_factor
         phi_factor = 0.65
 
-        inpts.wall_details()
-        inpts.reinf_details()
-        inpts.design_loads()
-
     def AxialCalc(self):
+        # Effective Wall Height:
+        Hwe = i.Hw * i.k
+        # Eccentricity:
+        ecc = 0.05*i.tw
+        # Additional Eccentricity:
+        ecc_add = ((Hwe**2)/(2500*tw))
+        # Check Effective Height Ratio:
+        eff_h_ratio = Hwe / tw
+        # Design Axial Strength of Wall, Phi.Nu:
+        phi_Nu = 0.65*((tw - 1.2*ecc-2*ecc_add) * 0.6*i.fc)
 
-
-
-
-
+    def ShearCalc(self):
+        # Shear Strength Excluding Wall Reinforcement
+        Vuc = 1
