e6f6ab3b6f321dccfd95553d6764b671adc504d0
daveh07 <david.hill87@outlook.com>
2021-08-24 13:58:04 +1000

    Updated Shear and Axial Calc

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ from modules import design_calcs
 from modules.design_calcs import *
 
 
+
 AxialCheck = design_calcs.WallAxialCapacity()
+ax_check = AxialCheck.AxialCalc()
+ax_round = round(ax_check, 2)
+ax_str = f"Axial Capacity of Wall Per Metre Run = {ax_round}kN/m"
 
 shearCheck = design_calcs.WallShearCapacity()
+sf_check = shearCheck.ShearCalc()
+sf_round = round(sf_check, 2)
+sf_str = f"Shear Capacity of Wall Per Metre Run = {sf_round}kN/m"
 
-print(str(round((AxialCheck.AxialCalc()), 2)) + "kN/m")
-print(str(round((shearCheck.ShearCalc()), 2)) + "kN/m")
+print(ax_str)
+print(sf_str)
